1ae81c4f37af76010530c7ec14e291f2fbd32791
berbagimadani <berbagimadani@gmail.com>
2025-06-21 06:09:47 +0700

    Add effect-based timeline support

--- src/effects/shake.js
+++ src/effects/shake.js
@@ -2,9 +2,16 @@ import { gsap } from 'gsap';
 
 export default function shake(target, params = {}, options = {}) {
   const { x = 10, y = 10, duration = 0.2, repeat = 5 } = params;
-  gsap.fromTo(target, { x: `-=${x}`, y: `-=${y}` }, {
-    x: `+=${x}`, y: `+=${y}`,
-    duration, repeat, yoyo: true,
-    ...options
-  });
+  return gsap.fromTo(
+    target,
+    { x: `-=${x}`, y: `-=${y}` },
+    {
+      x: `+=${x}`,
+      y: `+=${y}`,
+      duration,
+      repeat,
+      yoyo: true,
+      ...options
+    }
+  );
 }
