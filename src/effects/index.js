8be056afd267878cd61b4bf635d15f25d8cc3dc7
berbagimadani <berbagimadani@gmail.com>
2025-06-20 19:17:02 +0700

    Update timeline template with interactive animations

--- src/effects/index.js
+++ src/effects/index.js
@@ -3,11 +3,15 @@ import glitch from './glitch.js';
 import blur from './blur.js';
 import bounce from './bounce.js';
 import fadeInOut from './fadeInOut.js';
+import zoomIn from './zoomIn.js';
+import spinClose from './spinClose.js';
 
 export default {
   shake,
   glitch,
   blur,
   bounce,
-  fadeInOut
+  fadeInOut,
+  zoomIn,
+  spinClose
 };
---
+++ src/effects/zoomIn.js
@@ -0,0 +1,18 @@
+import { gsap } from 'gsap';
+
+export default function zoomIn(target, params = {}, options = {}) {
+  const {
+    from = 0,
+    to = 1,
+    alphaFrom = 0,
+    alphaTo = 1,
+    duration = 0.5,
+    ease = 'power2.out'
+  } = params;
+
+  gsap.fromTo(
+    target,
+    { scale: from, alpha: alphaFrom },
+    { scale: to, alpha: alphaTo, duration, ease, ...options }
+  );
+}
